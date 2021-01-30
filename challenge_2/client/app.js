b118afb739a76829cc49a202dbee148079c1cd77
Grant Taggart <granttaggartsf@gmail.com>
2021-01-29 22:13:14 -0800

    Trying to get server running

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,3 +1,44 @@
-console.log('working?')
+document.getElementById('import').onclick = function(e) {
+  var files = document.getElementById('selectFiles').value;
+  console.log(files);
+  e.preventDefault();
+}
+
+// window.addEventListener("load", function () {
+//   function sendData() {
+//     const XHR = new XMLHttpRequest();
+
+//     // Bind the FormData object and the form element
+//     const FD = new FormData(form);
+//     console.log(FD);
+//     // Define what happens on successful data submission
+//     XHR.addEventListener("load", function (event) {
+//       alert(event.target.responseText);
+//     });
+
+//     // Define what happens in case of error
+//     XHR.addEventListener("error", function (event) {
+//       alert('Oops! Something went wrong.');
+//     });
+
+//     // Set up our request
+//     XHR.open("POST", '/upload_json');
+
+//     // The data sent is what the user provided in the form
+//     XHR.send(FD);
+//   }
+
+//   // Access the form element...
+//   const form = document.getElementById("formdata");
+//   console.log(form);
+//   // ...and take over its submit event.
+//   form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     sendData();
+//   });
+// });
+
+
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,19 @@
-app.use(express.static('client')
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Serve client files
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/upload_json', (req, res) => {
+  console.log('*****!!!!req:', req, '******!!!!!res', res);
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
 
