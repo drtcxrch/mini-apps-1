d218150f97fe870cf87645a917c3f06c8acaa81c
Grant Taggart <granttaggartsf@gmail.com>
2021-01-30 18:27:44 -0800

    Complete json to CSV convertor

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -14,9 +14,47 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload_json', (req, res) => {
-  console.log('*****!!!!req:', req.body.json );
+  // console.log('*****!!!!req:', req.body.json );
+  // console.log(res);
+  var json = req.body.json;
+  // console.log('json', json);
+  var parsed = JSON.parse(json);
+  // console.log(parsed);
+  // console.log('csv', jsonToCSV(parsed));
+  console.log('csv', jsonToCSV(parsed));
   res.sendStatus(200);
 })
+
+var jsonToCSV = (json) => {
+
+  var fields = [];
+  var newLine = '';
+
+  var dataCollector = (json) => {
+
+    for (var field in json) {
+
+      if (fields.indexOf(field) === -1 && field !== 'children') {
+        fields.push(field);
+      }
+
+      if (field !== 'children') {
+        newLine += (json[field] + ',');
+      } else {
+        for (var i = 0; i < json.children.length; i++) {
+          newLine += '\r\n'
+          dataCollector(json.children[i]);
+        }
+      }
+    }
+  }
+
+  dataCollector(json);
+
+  var csv = fields.join(',') + '\r\n' + newLine;
+  return csv;
+
+}
 
 const port = 4000;
 
