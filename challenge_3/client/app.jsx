5e58253d78fb673f9cb54474bfaa0512b701396b
Grant Taggart <granttaggartsf@gmail.com>
2021-02-18 14:42:27 -0800

    Complete basic app structure

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,10 +1,32 @@
-
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-
+      currentForm: 0,
+      name: '',
+      email: '',
+      password: '',
+      line_1: '',
+      line_2: '',
+      city: '',
+      state: '',
+      zip_code: '',
+      phone_number: '',
+      credit_card: '',
+      expiry_date: '',
+      cvv: '',
+      billing_zip: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+
+  }
+
+  handleSubmit() {
+
   }
 
   render() {
@@ -12,6 +34,52 @@ class App extends React.Component {
       <div>Hi there!</div>
     )
   }
+}
+
+const Home = (props) => {
+  if (props.currentForm !== 0) {
+    return null;
+  }
+
+  return (
+    <button>Checkout</button>
+  )
+}
+
+
+
+const F1 = (props) => {
+  if (props.currentForm !== 1) {
+    return null;
+  }
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+const F2 = (props) => {
+  if (props.currentForm !== 2) {
+    return null;
+  }
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+const F3 = (props) => {
+  if (props.currentForm !== 3) {
+    return null;
+  }
+  return (
+    <div>
+
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
