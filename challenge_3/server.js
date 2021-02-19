29a22aa3066ac7a82d28ac5910db9122b88729b1
Grant Taggart <granttaggartsf@gmail.com>
2021-02-19 10:37:09 -0800

    Complete Bare Minimum requirements

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,18 +1,52 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'checkout'
+});
+
+connection.connect();
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/checkout', (req, res) => {
-  console.log('req', req.body)
+  var customer = req.body;
+  // let sql = "INSERT INTO customers (name, email, password, line_1, line_2, city, state, zip_code, phone_number, credit_card, expiry_date, cvv, billing_zip) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+  let sql = 'INSERT INTO customer SET ?';
+  let post = {
+    name: customer.name,
+    email: customer.email,
+    password: customer.password,
+    line_1: customer.line_1,
+    line_2: customer.line_2,
+    city: customer.city,
+    state: customer.state,
+    zip_code: customer.zip_code,
+    phone_number: customer.phone_number,
+    credit_card: customer.credit_card,
+    expiry_date: customer.expiry_date,
+    cvv: customer.cvv,
+    billing_zip: customer.billing_zip
+  }
+  connection.query(sql, post, (err, result) => {
+    if (err) {
+      throw err;
+    };
+
+    console.log('1 record inserted');
+  })
   res.send('Success!')
 })
 
