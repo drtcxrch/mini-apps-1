b594820c850f6997a8e35e4c19f4325eb7426565
Grant Taggart <granttaggartsf@gmail.com>
2021-02-18 16:39:27 -0800

    Complete form data collection

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -151,11 +151,53 @@ const F2 = (props) => {
   return (
     <div>
       <label>Line 1:</label>
+      <input
+        id="line_1"
+        name="line_1"
+        value={props.line_1}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>Line 2:</label>
+      <input
+        id="line_2"
+        name="line_2"
+        value={props.line_2}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>City:</label>
+      <input
+        id="city"
+        name="city"
+        value={props.city}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>State:</label>
+      <input
+        id="state"
+        name="state"
+        value={props.state}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>Zip:</label>
+      <input
+        id="zip_code"
+        name="zip_code"
+        value={props.zip_code}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>Phone #:</label>
+      <input
+        id="phone_number"
+        name="phone_number"
+        value={props.phone_number}
+        onChange={props.handleChange}
+      />
+      <br />
     </div>
   )
 }
@@ -167,9 +209,37 @@ const F3 = (props) => {
   return (
     <div>
       <label>Credit Card #:</label>
+      <input
+        id="credit_card"
+        name="credit_card"
+        value={props.credit_card}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>Expiration Date:</label>
+      <input
+        id="expiry_date"
+        name="expiry_date"
+        value={props.email}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>CVV:</label>
+      <input
+        id="cvv"
+        name="cvv"
+        value={props.cvv}
+        onChange={props.handleChange}
+      />
+      <br />
       <label>Billing Zip:</label>
+      <input
+        id="billing_zip"
+        name="billing_zip"
+        value={props.billing_zip}
+        onChange={props.handleChange}
+      />
+      <br />
     </div>
   )
 }
