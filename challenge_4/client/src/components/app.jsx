e02dd0e76729b0cc3049a8de763282c0821bb5d7
Grant Taggart <granttaggartsf@gmail.com>
2021-02-25 17:53:01 -0800

    Complete function to get a piece

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -7,9 +7,20 @@ class App extends React.Component {
         this.state = {
             rows: 6,
             columns: 7,
+            moves: [
+                {x: 0, y: 0, player: 'red'}
+            ]
         }
 
         this.createBoard = this.createBoard.bind(this);
+        this.getPiece = this.getPiece.bind(this);
+    }
+
+    getPiece(x, y) {
+        const list = this.state.moves.filter((item) => {
+            return (item.x === x && item.y === y);
+        });
+        return list[0];
     }
 
     createBoard() {
@@ -19,12 +30,17 @@ class App extends React.Component {
         for (var i = 0; i < this.state.rows; i++) {
             const columnViews = [];
             for (var j = 0; j < this.state.columns; j++) {
+
+                const piece = this.getPiece(j, i);
+
                 columnViews.push(
                     <div
                         key={'column-' + j}
-                        style={{width: '8vw', height: '8vw', backgroundColor: '#00a8ff', display: 'flex', padding: 5}}
+                        style={{width: '8vw', height: '8vw', backgroundColor: 'blue', display: 'flex', padding: 5}}
                     >
-                        <div style={{borderRadius: '50%', backgroundColor: 'white', flex: 1}}></div>
+                        <div style={{borderRadius: '50%', backgroundColor: 'white', flex: 1, display: 'flex'}}>
+                            {piece ? <div style={{ backgroundColor: piece.player, flex: 1, borderRadius: '50%', border: '1px solid'}}/> : undefined}
+                        </div>
                     </div>
                 )
             }
